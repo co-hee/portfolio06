04122148fa60952aaf40646fd64c315e45e184ee
Seonghee Yun <104363065+intacthee@users.noreply.github.com>
2022-12-27 17:45:56 +0900

    Add files via upload

---
+++ js/portfolio06.js
@@ -0,0 +1,37 @@
+$(function () {
+
+    $('.mainVisual .main_slide .list').slick({
+        // dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        pauseOnHover: false,
+        pauseOnFocus: false
+    });
+
+
+    $('.event_slide_wrap').slick({
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
