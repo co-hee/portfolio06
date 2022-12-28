fa2bcea87a260f971a6592ced7e385ad2ffe093d
Seonghee Yun <104363065+intacthee@users.noreply.github.com>
2022-12-28 17:45:22 +0900

    Add files via upload

--- js/portfolio06.js
+++ js/portfolio06.js
@@ -1,5 +1,19 @@
 $(function () {
 
+    // $('html').smoothScroll({
+
+    // });
+
+    $(window).scroll(function (evt) {
+        var y = $(this).scrollTop();
+        if (y > 0) {
+            $('.header').addClass('on');
+
+        } else {
+            $('.header').removeClass('on');
+        }
+    });
+
     $('.mainVisual .main_slide .list').slick({
         // dots: true,
         arrows: false,
@@ -11,14 +25,26 @@ $(function () {
 
 
     $('.event_slide_wrap').slick({
-        arrows: false,
+        //arrows: false,
         autoplay: false,
-        slidesToShow: 3,
+        slidesToShow: 4,
     });
 
-    // $('.dining_slide_wrap .dining_slide .slider_img_cont .list').slick({
+    $('.slider_img_cont .list').slick({});
 
-    // });
+
+    $('.main_dining .tab li').on('click', function (e) {
+        e.preventDefault();
+        var idx = $(this).index();
+        $('.dining_slide_wrap .dining_slide>div').eq(idx).addClass('on').siblings().removeClass('on');
+    });
+
+
+    $('.main_special .tab li').on('click', function (e) {
+        e.preventDefault();
+        var idx = $(this).index();
+        $('.special_slide_wrap .special_slide>div').eq(idx).addClass('on').siblings().removeClass('on');
+    });
 
 
 
